65124abd2f0d14905645ef255833b3ab1e615928
yshsir <624102318@qq.com>
2020-04-29 00:12:22 +0800

    接口封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,43 @@
 // ./pages/home/home.js
+import request from '../../service/network.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    //原生分装的request发送网络请求
+    this.gey_data_origin()
 
+    //使用封装的request发送网络请求
+    // Promise最大的好处防止出现回调地狱
+    request({
+      url: 'http://152.136.185.210:8000/api/h8/recommend'
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  gey_data_origin() {
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/h8/recommend',
+      success: (res)=> {
+        console.log(res)
+      }
+    })
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   methd: 'post',
+    //   data: {
+    //     name: 'why',
+    //     age: 18
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   },
+    //   fail: (err) => {
+    //     console.log(err)
+    //   }
+    // })
   }
 })
