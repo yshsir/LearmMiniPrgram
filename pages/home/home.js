88938f20dd36ba36454fd2ef0c1110446eff9d2d
yshsir <624102318@qq.com>
2020-04-29 10:16:02 +0800

    弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,53 @@
 // ./pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handShowModal() {
+    wx.showModal({
+      title: '标题',
+      content: '内容',
+      showCancel: true,
+      cancelText: '返回',
+      cancelColor: 'blue',
+      confirmText: '前进',
+      confirmColor: 'red',
+      success: function(res) {
+        if (res.cancel) {
+          console.log('点击了取消')
+        }
+        if (res.confirm) {
+          console.log('点击了确定')
+        }
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handShowLoading() {
+    wx.showLoading({
+      title: 'jiazai',
+      mask: true,
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+    setTimeout(() => {
+      //手动加载显示消失
+      wx.hideLoading()
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handShowActive() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      success: function(res) {
+        console.log(res)
+        // switch(res.tapIndex == 0){}//判断
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options) {
+    return {
+      title: '你好',
+      path: '/pages/about/about',
+      imageUrl: ''
+    }
   }
 })
